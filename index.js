b0bd631705c1bd975e912e1978f0d16b8c47e985
Jane Jeon <JaneJeon9719@gmail.com>
2018-11-26 17:05:25 +0900

    fixed user input handling

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
 #!/usr/bin/env node
-const { prompt } = require("enquirer")
-const profile = require("./lib/profile")
+const { Input, Dir, Profile } = require("./lib")
 
 const run = async () => {
-  const config = await profile.default()
+  const config = await Profile.default()
+  const input = await Input.read(config)
 
-  const result = await prompt([
-    {
-      type: "input",
-      name: "directory",
-      message: "Which directory to search for git repos?",
-      initial: __dirname,
-      validate: val => val.trim().length
-    },
-    {
-      type: "form",
-      name: "old",
-      message: "Old git config (usually author name and/or email is enough):",
-      choices: [
-        { name: "AUTHOR_NAME", message: "Author name" },
-        { name: "AUTHOR_EMAIL", message: "Author email" },
-        { name: "COMMITTER_NAME", message: "Committer name" },
-        { name: "COMMITTER_EMAIL", message: "Committer email" }
-      ],
-      validate: val => Object.keys(val).length
-    },
-    {
-      type: "form",
-      name: "new",
-      message: "New git config (usually author name and/or email is enough):",
-      choices: [
-        {
-          name: "AUTHOR_NAME",
-          message: "Author name",
-          initial: config["user.name"]
-        },
-        {
-          name: "AUTHOR_EMAIL",
-          message: "Author email",
-          initial: config["user.email"]
-        },
-        { name: "COMMITTER_NAME", message: "Committer name" },
-        { name: "COMMITTER_EMAIL", message: "Committer email" }
-      ],
-      validate: val => Object.keys(val).length
-    },
-    { type: "confirm", name: "force", message: "Force push changes to remote?" }
-  ])
+  const repos = await Dir.getRepos(input.dir)
+  if (!repos.length) {
+    console.log("No repos found in specified directory")
+    process.exit(1)
+  }
 
-  console.log(result)
+  console.log(repos)
 }
 
 run()
